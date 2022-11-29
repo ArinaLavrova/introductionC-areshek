8134813570956b2efb6e27d9de7ae398d7cbb80a
Арина <arina.lavrova.93@gmail.com>
2022-11-29 18:44:23 +0300

    Lesson 4

---
+++ BasicAlgorithms/005-swithc.csproj/Program.cs
@@ -0,0 +1,37 @@
+﻿//С клавиатуры вводится число - день недели. Определить день недели
+
+int day=1;
+if (day==1) System.Console.WriteLine("Понедельник");
+    else
+        if (day==2) System.Console.WriteLine("Вторник");
+            else
+                if (day==3) System.Console.WriteLine("Среда");
+
+switch(day)
+{
+    case 1:
+        System.Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        System.Console.WriteLine("Вторник");
+        break;
+    case 3:
+        System.Console.WriteLine("Среда");
+        break;
+    default:
+    System.Console.WriteLine("не правельный ввод");
+        break;
+}
+
+
+metka1:
+System.Console.WriteLine();
+goto metka1;//оператор безусловного перехода
+//GOTO - нельзя использовать
+
+
+void Test()
+{
+    metka2:
+        System.Console.WriteLine();
+}
---
+++ BasicAlgorithms/009-RandomArray(Массив из случайных чисел)/Program.cs
@@ -0,0 +1,35 @@
+﻿// Массив из случайных чисел
+
+/*
+int[] RandomInArray(int size=10,int min=0, int max=100)
+{
+    int[] a=new int[size];
+    Random random=new Random();
+    for(int i=0;i<size;i++)
+        a[i]=random.Next(min,max);
+    return a;
+}
+*/
+
+void RandomInArray(out int[] a,int size=10,int min=0,int max=100)
+{
+    a=new int[size];
+    Random random=new Random();
+    for(int i=0;i<size;i++)
+        a[i]=random.Next(min,max);
+}
+
+
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i],5}");
+}
+
+
+
+int[] m=new int[3];
+RandomInArray(out m);
+Print(m);
+
+//
